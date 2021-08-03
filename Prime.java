c54eb3863220bbbc3e9f2fcbcabb7db676974b37
tippana veera charishma <49685749+charishma15@users.noreply.github.com>
2021-08-03 17:19:46 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -5,7 +5,7 @@ public class Prime {
     int n = 29;
     boolean flag = false;
     for (int i = 2; i <= n / 2; ++i) {
-      // condition for nonprime number
+   
       if (n % i == 0) {
         flag = true;
         break;
