50de9fbff04f9dd6b7d2458f2c16b827ac364fab
tippana veera charishma <49685749+charishma15@users.noreply.github.com>
2021-08-03 17:13:51 +0530

    Update MultipleOperations.java

--- MultipleOperations.java
+++ MultipleOperations.java
@@ -6,14 +6,14 @@ class MultipleOperations {
     char operator;
     Double n1, n2, result;
 
-    // create an object of Scanner class
+   
     Scanner input = new Scanner(System.in);
 
-    // ask users to enter operator
+   
     System.out.println("Choose any operator of your choice: +, -, *, or /");
     operator = input.next().charAt(0);
 
-    // ask users to enter numbers
+   
     System.out.println("Enter first number");
     n1 = input.nextDouble();
 
@@ -22,25 +22,24 @@ class MultipleOperations {
 
     switch (operator) {
 
-      // performs addition between numbers
+      
       case '+':
         result = n1 + n2;
         System.out.println(n1 + " + " + n2 + " = " + result);
         break;
 
-      // performs subtraction between numbers
       case '-':
         result = n1 - n2;
         System.out.println(n1 + " - " + n2 + " = " + result);
         break;
 
-      // performs multiplication between numbers
+     
       case '*':
         result = n1 * n2;
         System.out.println(n1 + " * " + n2 + " = " + result);
         break;
 
-      // performs division between numbers
+  
       case '/':
         result = n1/ n2;
         System.out.println(n1 + " / " + n2 + " = " + result);
