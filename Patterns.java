83ef0e8ea5b0bb6586e77184a1455b44c5bd4e91
tippana veera charishma <49685749+charishma15@users.noreply.github.com>
2021-08-03 17:19:00 +0530

    Update Patterns.java

--- Patterns.java
+++ Patterns.java
@@ -4,10 +4,10 @@ public class Patterns
 {
     public static void main(String[] args)
     {
-        // Create a new Scanner object
+        
         Scanner scanner = new Scanner(System.in);
 
-        // Get the number of rows from the user
+        
         System.out.println("Enter the number of lines needed to print the "*" pattern ");
 
         int lines = scanner.nextInt();
@@ -15,12 +15,12 @@ public class Patterns
         
         for (int i=1; i<=lines; i++) 
         { 
-            // Print star in decreasing order 
+             
             for (int k=lines; k>=i; k--)
             {
                 System.out.print("*");
             }
-            // Print space in increasing order
+        
             for (int j=1; j<i; j++)
             {
                 System.out.print(" ");
